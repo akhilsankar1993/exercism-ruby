73bd6dd6c25b9ec75113bdb20f7a5fc7b176181c
akhilsankar1993 <akhil.sankar@wink.com>
2018-04-02 11:59:29 -0400

    hello world and hamming(v2)

---
+++ hamming/hamming.rb
@@ -0,0 +1,16 @@
+class Hamming
+  def self.compute(first_str, second_str)
+    raise ArgumentError.new unless first_str.length == second_str.length
+
+    first_chars  = first_str.split('')
+    second_chars = second_str.split('')
+
+    # select.with_index works because we can chain enumerators in ruby
+    # Ref: https://rossta.net/blog/what-is-enumerator.html
+    first_chars.select.with_index { |char,i| char != second_chars[i] }.count
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
