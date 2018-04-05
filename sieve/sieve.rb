ae3029c66f102238a3c445631cfb6f1a68075dd2
akhilsankar1993 <akhil.sankar@wink.com>
2018-04-05 11:28:06 -0400

    sieve (v0) & readme

---
+++ sieve/sieve.rb
@@ -0,0 +1,21 @@
+class Sieve
+  def initialize(input)
+    @input = input
+  end
+
+  def primes
+    expected = []
+    expected.push(2) if @input >= 2
+
+    (1..@input).each do |num|
+      if num % 
+    end
+
+    expected
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+  SOLUTION_VERSION = 1
+end
